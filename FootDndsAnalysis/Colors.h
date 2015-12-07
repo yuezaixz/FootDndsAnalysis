c6a0010c2a94b72fcc4622eff1c470c2ee56496b
yuezaixz <303178394@qq.com>
2015-12-07 14:42:07 +0800

    create constants package

---
+++ FootDndsAnalysis/Colors.h
@@ -0,0 +1,14 @@
+//
+//  Colors.h
+//  Runmove
+//
+//  Created by yufei on 14/12/19.
+//  Copyright (c) 2014年 Maytaste. All rights reserved.
+//
+
+#ifndef Foot_Colors_h
+#define Foot_Colors_h
+
+#define FOOT_DARK_GRAY ([UIColor colorWithRed:216.0/255.0 green:216.0/255.0 blue:216.0/255.0 alpha:1.0])
+
+#endif
---
+++ FootDndsAnalysis/Notifications.h
@@ -0,0 +1,12 @@
+//
+//  Notifications.h
+//  Runmove
+//
+//  Created by yufei on 14/12/12.
+//  Copyright (c) 2014年 Maytaste. All rights reserved.
+//
+
+#ifndef Foot_Notifications_h
+#define Foot_Notifications_h
+
+#endif
